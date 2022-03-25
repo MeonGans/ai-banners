a683a01ed1b2e3e42002bada5c4919f1a9d8b080
Steve <meon.gans@gmail.com>
2022-03-25 13:45:06 +0200

    cors update 3 /API return

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -26,27 +26,27 @@ class RouteServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->configureRateLimiting();
-
-        $this->routes(function () {
-            Route::middleware('api')
-                ->group(base_path('routes/api.php'));
-
-            Route::prefix('web')
-            ->middleware('web')
-                ->group(base_path('routes/web.php'));
-        });
-
 //        $this->configureRateLimiting();
 //
 //        $this->routes(function () {
-//            Route::prefix('api')
-//                ->middleware('api')
+//            Route::middleware('api')
 //                ->group(base_path('routes/api.php'));
 //
-//            Route::middleware('web')
+//            Route::prefix('web')
+//            ->middleware('web')
 //                ->group(base_path('routes/web.php'));
 //        });
+
+        $this->configureRateLimiting();
+
+        $this->routes(function () {
+            Route::prefix('api')
+                ->middleware('api')
+                ->group(base_path('routes/api.php'));
+
+            Route::middleware('web')
+                ->group(base_path('routes/web.php'));
+        });
     }
 
     /**
