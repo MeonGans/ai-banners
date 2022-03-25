d3e566315285968927ce498fff0fbd5752ca623a
Steve <meon.gans@gmail.com>
2022-03-25 13:16:22 +0200

    Add Cors middleware

--- routes/api.php
+++ routes/api.php
@@ -12,31 +12,38 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::middleware('cors')->group(function () {
 
-route::get('/', function() {
-   return 'HELLO';
-});
+    route::get('/', function () {
+        return 'HELLO';
+    });
 
-Route::post('register', [\App\Http\Controllers\RegisterController::class, 'register'])->name('api.register');
-Route::post('login', [\App\Http\Controllers\RegisterController::class, 'login'])->name('api.login');
+    route::post('/hello', function () {
+        return 'HELLO';
+    });
 
-Route::middleware('auth:api')->group(function () {
-    Route::get('logout', [\App\Http\Controllers\RegisterController::class, 'logout'])->name('api.logout');
-});
+    Route::post('register', [\App\Http\Controllers\RegisterController::class, 'register'])->name('api.register');
+    Route::post('login', [\App\Http\Controllers\RegisterController::class, 'login'])->name('api.login');
 
-Route::middleware('admin')->group(function () {
+    Route::middleware('auth:api')->group(function () {
+        Route::get('logout', [\App\Http\Controllers\RegisterController::class, 'logout'])->name('api.logout');
+    });
+
+    Route::middleware('admin')->group(function () {
+        Route::apiResources([
+            //Создание, обновление и удаление баннеров, категорий
+            'banners' => \App\Http\Controllers\BannerController::class,
+            'categories' => \App\Http\Controllers\CategoryController::class,
+        ], ['except' => ['show', 'index']]);
+        //Восстановление удалённой категории
+        Route::post('categories/{category}/restore', [\App\Http\Controllers\CategoryController::class, 'restore']);
+    });
+
     Route::apiResources([
-        //Создание, обновление и удаление баннеров, категорий
         'banners' => \App\Http\Controllers\BannerController::class,
         'categories' => \App\Http\Controllers\CategoryController::class,
-    ], ['except' => ['show', 'index']]);
-    //Восстановление удалённой категории
-    Route::post('categories/{category}/restore', [\App\Http\Controllers\CategoryController::class, 'restore']);
+    ], ['except' => ['update', 'store', 'destroy']]);
+
+    Route::get('categories/{category}/{option?}', [\App\Http\Controllers\CategoryController::class, 'show']);
+
 });
-
-Route::apiResources([
-    'banners' => \App\Http\Controllers\BannerController::class,
-    'categories' => \App\Http\Controllers\CategoryController::class,
-], ['except' => ['update', 'store', 'destroy']]);
-
-Route::get('categories/{category}/{option?}', [\App\Http\Controllers\CategoryController::class, 'show']);
