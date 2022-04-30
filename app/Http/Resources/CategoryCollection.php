d657b5221f326bdd9034abddfc176cd34c550731
Steve <meon.gans@gmail.com>
2022-04-30 17:17:10 +0300

    Banner and Category Collection add

---
+++ app/Http/Resources/CategoryCollection.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+class CategoryCollection extends ResourceCollection
+{
+    public static $wrap = '';
+    /**
+     * Transform the resource collection into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'categories' => $this->collection,
+            'categoriesCount' =>$this->count(),
+        ];
+    }
+}
